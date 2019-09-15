ad609ee70adf2a3cb5b0fd90c5f857fdf7efd812
Cader <Cader.Hancock@gmail.com>
2019-09-15 11:49:38 -0700

    init

---
+++ src/main/java/gq/cader/realfakestoreserver/RealfakestoreserverApplication.java
@@ -0,0 +1,13 @@
+package gq.cader.realfakestoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealfakestoreserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealfakestoreserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gq/cader/realfakestoreserver/RealfakestoreserverApplicationTests.java
@@ -0,0 +1,16 @@
+package gq.cader.realfakestoreserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealfakestoreserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
