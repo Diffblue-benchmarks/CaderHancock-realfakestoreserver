8834943582ea432337cb0f1c090efcf3ff17dcda
Cader <Cader.Hancock@gmail.com>
2019-09-16 10:02:56 -0700

    Cust repo

---
+++ src/main/java/gq/cader/realfakestoreserver/CustomerRepository.java
@@ -0,0 +1,10 @@
+package gq.cader.realfakestoreserver;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Override
+    List<Customer> findAll();
+}
