657bcdb33140d5c41614d6f88a1d22d2c98de283
Cader <cader.hancock@gmail.com>
2019-09-15 20:33:41 -0700

    H2 console enabled

--- src/test/java/gq/cader/realfakestoreserver/RealfakestoreserverApplicationTests.java
+++ src/test/java/gq/cader/realfakestoreserver/RealfakestoreserverApplicationTests.java
@@ -1,10 +1,8 @@
 package gq.cader.realfakestoreserver;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.DisabledIf;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class RealfakestoreserverApplicationTests {
 
     @Test
-    @Disabled
     public void contextLoads() {
     }
 
--- src/test/java/gq/cader/realfakestoreserver/test.java
+++ src/test/java/gq/cader/realfakestoreserver/test.java
@@ -9,15 +9,22 @@ public class test {
 
     @Test
     public void testCart() {
+        Product p1 = new Product();
         Product p = new Product();
-        p.setName("Dildo");
+        p.setName("ok");
+        p.setPrice(12.3);
+        p1.setName("ko");
+        p1.setPrice(45.6);
         ShoppingCart shop = new ShoppingCart();
         shop.put(p, 1);
         shop.put(p, 10);
         shop.put(p, -5);
+        shop.put(p1, 1);
         assertTrue(shop.getProductQuantityMap().get(p).equals(6));
         shop.put(p, -7);
         assertTrue(shop.getProductQuantityMap().get(p).equals(0));
+        shop.put(p, 1);
+//        assertTrue(shop.getTotalPrice() == 57.9);
     }
 
 }
