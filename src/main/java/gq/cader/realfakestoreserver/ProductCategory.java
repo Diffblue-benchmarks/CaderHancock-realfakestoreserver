461c251ba2c57d063fd8db9ba34478328937e48c
Cader <cader.hancock@gmail.com>
2019-09-15 20:11:51 -0700

    Deleted useless imports

--- src/main/java/gq/cader/realfakestoreserver/ProductCategory.java
+++ src/main/java/gq/cader/realfakestoreserver/ProductCategory.java
@@ -1,9 +1,5 @@
 package gq.cader.realfakestoreserver;
 
-import org.springframework.messaging.handler.annotation.SendTo;
-
-import javax.persistence.Entity;
-
 public enum ProductCategory {
     BOOKS, CLOTHING, COSMETICS, HOME, OUTDOORS, TECHNOLOGY, TOYS
 
