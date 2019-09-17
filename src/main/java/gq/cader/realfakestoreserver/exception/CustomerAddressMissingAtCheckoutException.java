bcc4746f790566ecd3c380bb43e82c52bfc049f9
Cader <cader.hancock@gmail.com>
2019-09-17 10:39:09 -0700

    Added exception package w/ 4 new exceptions
    CustomerNotFoundException
    ProductNotFoundException
    ProductInventoryException
    CustomerAddressMissingAtCheckoutException

---
+++ src/main/java/gq/cader/realfakestoreserver/exception/CustomerAddressMissingAtCheckoutException.java
@@ -0,0 +1,22 @@
+package gq.cader.realfakestoreserver.exception;
+
+public class CustomerAddressMissingAtCheckoutException extends RuntimeException {
+    public CustomerAddressMissingAtCheckoutException() {
+    }
+
+    public CustomerAddressMissingAtCheckoutException(String message) {
+        super(message);
+    }
+
+    public CustomerAddressMissingAtCheckoutException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomerAddressMissingAtCheckoutException(Throwable cause) {
+        super(cause);
+    }
+
+    public CustomerAddressMissingAtCheckoutException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/gq/cader/realfakestoreserver/exception/ProductInventoryException.java
@@ -0,0 +1,24 @@
+package gq.cader.realfakestoreserver.exception;
+
+public class ProductInventoryException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ProductInventoryException() {
+    }
+
+    public ProductInventoryException(String message) {
+        super(message);
+    }
+
+    public ProductInventoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProductInventoryException(Throwable cause) {
+        super(cause);
+    }
+
+    public ProductInventoryException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
