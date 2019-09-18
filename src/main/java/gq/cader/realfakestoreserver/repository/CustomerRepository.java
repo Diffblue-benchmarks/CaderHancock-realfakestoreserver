fbeee51c5fd18cdbe0ba0dcefe4026ac5b7896df
Cader <cader.hancock@gmail.com>
2019-09-18 12:43:20 -0700

    Prevented Customer POST from adding entities with duplicate emails.

--- src/main/java/gq/cader/realfakestoreserver/repository/CustomerRepository.java
+++ src/main/java/gq/cader/realfakestoreserver/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package gq.cader.realfakestoreserver.repository;
 import gq.cader.realfakestoreserver.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
 }
--- src/main/java/gq/cader/realfakestoreserver/service/CustomerService.java
+++ src/main/java/gq/cader/realfakestoreserver/service/CustomerService.java
@@ -2,6 +2,7 @@ package gq.cader.realfakestoreserver.service;
 
 import gq.cader.realfakestoreserver.entity.Customer;
 import gq.cader.realfakestoreserver.entity.ShoppingCart;
+import gq.cader.realfakestoreserver.exception.CustomerNotFoundException;
 import gq.cader.realfakestoreserver.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,18 +26,20 @@ public class CustomerService {
         this.shoppingCartService = shoppingCartService;
     }
 
-    void createCustomer(String firstName, String lastName) {
+    void createCustomer(String firstName, String lastName, String email) {
 
         Customer customer = new Customer();
         customer.setFirstName(firstName);
         customer.setLastName(lastName);
+        customer.setEmail(email);
         customer.setShoppingCart(new ShoppingCart());
-        customerRepository.save(customer);
-        LOG.info("Created Customer: " + customer.toString());
+        this.postNewCustomer(customer);
+
+
     }
 
     public void checkOutCustomer(Integer customerId) {
-
+        //TODO Implement this
     }
 
     public Customer findById(Integer customerId) {
@@ -44,9 +47,19 @@ public class CustomerService {
         LOG.info("Querying CustomerRepository for ID:" + customerId.toString());
         Optional<Customer> result = customerRepository.findById(customerId);
         return result.orElseGet(() -> {
-            LOG.warn("ID:" + customerId.toString() + " not found. Returning empty Customer.");
-            return new Customer();
+            throw new CustomerNotFoundException("ID:" + customerId.toString() + " not found.");
         });
+    }
+
+    private Customer postNewCustomer(Customer customer) {
+        if (customerRepository.findByEmail(customer.getEmail()).isPresent()) {
+            LOG.warn("Customer: " + customer.toString() + " Already exists");
+            return customerRepository.findByEmail(customer.getEmail()).get();
+        } else {
+            LOG.info("Created Customer: " + customer.toString());
+            return customerRepository.save(customer);
+
+        }
     }
 
 }
