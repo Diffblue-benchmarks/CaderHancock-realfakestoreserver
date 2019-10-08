5f0681f49ab9af59562fef30348fa887985bc8a5
Diffblue Assistant <42613982+diffblue-assistant@users.noreply.github.com>
2019-10-08 18:35:50 +0000

    Add Unit Tests for gq.cader.realfakestoreserver.model.entity.ProductCategory
    These tests were written using Diffblue Cover

---
+++ src/test/java/gq/cader/realfakestoreserver/model/entity/ProductCategoryTest.java
@@ -0,0 +1,25 @@
+package gq.cader.realfakestoreserver.model.entity;
+
+import gq.cader.realfakestoreserver.model.entity.ProductCategory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+public class ProductCategoryTest {
+
+  @Rule public final Timeout globalTimeout = new Timeout(10000);
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void valueOfInputNotNullOutputIllegalArgumentException() {
+
+    // Act
+    thrown.expect(IllegalArgumentException.class);
+    ProductCategory.valueOf("foo");
+
+    // The method is not expected to return due to exception thrown
+  }
+}
